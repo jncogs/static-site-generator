8ca072fc2fb87f611f44710fca4e225674e72fe7
jncogs <cogswell.jesse@gmail.com>
2025-07-17 19:09:05 -0500

    Added docs directory and build.sh

--- src/main.py
+++ src/main.py
@@ -1,18 +1,23 @@
 import os
+import sys
 import shutil
 
 from htmlfunctions import markdown_to_html_node
 from mdfunctions import extract_title
 
 def main():
-    create_public_dir()
-    #generate_page("content/index.md","template.html", "public/index.html")
-    generate_page_recursive("content", "template.html", "public")
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    else:
+        basepath = "/"
+    print(basepath)
+    create_docs_dir()
+    generate_page_recursive("./content", "./template.html", "./docs", basepath)
 
-def create_public_dir():
+def create_docs_dir():
     cwd = os.getcwd()
-    static_path = os.path.join(cwd, "static")
-    public_path = os.path.join(cwd, "public")
+    static_path = os.path.join(cwd, "./static")
+    public_path = os.path.join(cwd, "./docs")
 
     #print(f"Working dir: {cwd}")
     #print(f"Source: {static_path}")
@@ -39,7 +44,7 @@ def copy_dir_contents(source, dest):
             #print(f"Copying directory to {dest_path}")
             copy_dir_contents(source_path, dest_path)
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     page = None
     print(f"Generating page from {from_path} to {dest_path}, using {template_path}")
     with open(from_path) as file:
@@ -54,6 +59,8 @@ def generate_page(from_path, template_path, dest_path):
 
     page = template.replace("{{ Title }}", title)
     page = page.replace("{{ Content }}", content)
+    page = page.replace('href="/', f'href="{basepath}')
+    page = page.replace('src="/', f'src="{basepath}')
 
     if not os.path.exists(os.path.dirname(dest_path)):
         os.mkdir(os.path.dirname(dest_path))
@@ -62,17 +69,17 @@ def generate_page(from_path, template_path, dest_path):
         with open(dest_path, "w") as file:
             file.write(page)
 
-def generate_page_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_page_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     directory = os.listdir(dir_path_content)
     for dir in directory:
         if os.path.isfile(os.path.join(dir_path_content, dir)):
             file_path = os.path.join(dir_path_content, dir)
             filename, extension = os.path.splitext(file_path)
             if extension == ".md":
-                generate_page(file_path, template_path, os.path.join(dest_dir_path, "index.html"))
+                generate_page(file_path, template_path, os.path.join(dest_dir_path, "index.html"), basepath)
         else:
             if not os.path.exists(os.path.join(dest_dir_path, dir)):
                 os.mkdir(os.path.join(dest_dir_path, dir))
-            generate_page_recursive(os.path.join(dir_path_content, dir), template_path, os.path.join(dest_dir_path, dir))
+            generate_page_recursive(os.path.join(dir_path_content, dir), template_path, os.path.join(dest_dir_path, dir), basepath)
 
 main()
